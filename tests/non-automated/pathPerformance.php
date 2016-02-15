0fdcf19f5602a574acafcf3fd2ae4e6494a94210
Robbert Müller <spam.me@grols.ch>
2016-02-15 09:23:05 +0100

    fix pathperformance script

--- tests/non-automated/pathPerformance.php
+++ tests/non-automated/pathPerformance.php
@@ -1,6 +1,6 @@
 <?php
     // performance test
-    require_once( __DIR__.'/../bootstrap.php' );
+    require_once( __DIR__.'/../../src/path.php');
 
     // 100.000 times the same path - 1.9s naive implementation - 0.12s with cache
     $inputPath = 'a/../path//going/../somewhere';
